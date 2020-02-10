df1e51e8bfbcd126a63ce563196571e8b9a3d07a
Bogdan <bogdan.oloeriu@gmail.com>
2020-02-10 23:50:50 +0100

    functional drag and drop

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components'
+import { Draggable } from 'react-beautiful-dnd'
 
 const Container = styled.div`
     border: 1px solid lightgrey;
@@ -8,8 +9,20 @@ const Container = styled.div`
     margin-bottom: 8px;
 `
 
-export default class Task extends React.Component{
-    render(){
-        return <Container>this.props.task.content</Container>
-    }
+export default class Task extends React.Component {
+  render() {
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {(provided) => (
+          <Container
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    )
+  }
 }
